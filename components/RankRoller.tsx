6fb33366128e6f7c88e5a4b2af180207a1509d5b
Avery Opela <averyopela1@gmail.com>
2026-02-03 18:59:12 -0600

    Add 'Unlock Runes' button to cheat menu
    - Adds Rare 1 rank to collected ranks to unlock runes
    - Button only shows when runes are not yet unlocked
    - Available on both main screen and runes screen cheat menus

--- components/RankRoller.tsx
+++ components/RankRoller.tsx
@@ -1631,6 +1631,14 @@ export default function RankRoller() {
                   />
                 </div>
               </div>
+              {!runesUnlocked && (
+                <button
+                  onClick={() => setCollectedRanks(prev => new Set([...prev, 20]))}
+                  style={{...styles.cheatCloseBtn, backgroundColor: '#9333ea', marginBottom: '10px'}}
+                >
+                  Unlock Runes
+                </button>
+              )}
               <button
                 onClick={() => setShowCheatMenu(false)}
                 style={styles.cheatCloseBtn}
@@ -2525,6 +2533,14 @@ export default function RankRoller() {
                 />
               </div>
             </div>
+            {!runesUnlocked && (
+              <button
+                onClick={() => setCollectedRanks(prev => new Set([...prev, 20]))}
+                style={{...styles.cheatCloseBtn, backgroundColor: '#9333ea', marginBottom: '10px'}}
+              >
+                Unlock Runes
+              </button>
+            )}
             <button
               onClick={() => setShowCheatMenu(false)}
               style={styles.cheatCloseBtn}
